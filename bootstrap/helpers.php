1001cc1ae9712f0f60e347dd0cb6639fb7585520
sebastiankennedy <sebastiankennedy@foxmail.com>
2021-01-11 13:45:41 +0800

    feat: 新增 hash_equals 函数

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,1 +1,22 @@
 <?php
+
+if ( ! function_exists('hash_equals')) {
+    function hash_equals($a, $b)
+    {
+        if ( ! is_string($a) || ! is_string($b)) {
+            return false;
+        }
+
+        $length = mb_strlen($a);
+        if ($length !== mb_strlen($b)) {
+            return false;
+        }
+
+        $status = 0;
+        for ($i = 0; $i < $length; ++$i) {
+            $status |= ord($a[$i]) ^ ord($b[$i]);
+        }
+
+        return 0 === $status;
+    }
+}
